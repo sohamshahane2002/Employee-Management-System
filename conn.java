5d2b020b73d82ab6db937f8bc945d4986468dd41
sohamshahane2002 <134382971+sohamshahane2002@users.noreply.github.com>
2024-09-01 00:51:23 +0530

    Add files via upload

---
+++ conn.java
@@ -0,0 +1,23 @@
+package EmployeeManagementSystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class conn {
+
+    Connection connection;
+    Statement statement;
+
+    public conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeemanagement","root","soham@2002");
+            statement = connection.createStatement();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
